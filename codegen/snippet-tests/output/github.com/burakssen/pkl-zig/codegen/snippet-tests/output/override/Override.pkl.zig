11dd34d791f3b8e67578aead8746434679eb7fa6
Burak Şen <buraksen7@hotmail.com>
2026-08-24 00:40:31 +0200

    fix(codegen): generated loadFromPath unwraps Evaluator.InitResult

--- codegen/snippet-tests/output/github.com/burakssen/pkl-zig/codegen/snippet-tests/output/override/Override.pkl.zig
+++ codegen/snippet-tests/output/github.com/burakssen/pkl-zig/codegen/snippet-tests/output/override/Override.pkl.zig
@@ -22,7 +22,10 @@ pub fn Override(comptime FooType: type) type { return struct {
     };
 
     pub fn loadFromPath(allocator: std.mem.Allocator, io: std.Io, path: []const u8) !@This() {
-        var evaluator = try pkl.Evaluator.init(io, allocator, .{});
+        var evaluator = switch (try pkl.Evaluator.init(io, allocator, .{})) {
+            .evaluator => |evaluator| evaluator,
+            .failed => return error.CreateEvaluatorFailed,
+        };
         defer evaluator.deinit();
         return evaluator.loadFromPath(@This(), path);
     }
